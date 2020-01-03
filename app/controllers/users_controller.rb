96feadbc145cb2b060acf28461d1315c84844d0a
name <sample@tutorial.com>
2020-01-03 10:07:40 +0000

    Finish user edit index destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page],per_page: 20)
+  end
   
   def show
     @user = User.find(params[:id])
@@ -19,10 +26,47 @@ class UsersController < ApplicationController
     end
    end
    
+   def edit
+   end
+   
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "削除しました。"
+    redirect_to users_url
+   end
+   
+   
+   def update
+    if @user.update_attributes(user_params)
+     flash[:success] = "ユーザー情報を更新しました"
+     redirect_to @user
+    else
+     render 'edit'
+    end
+   end
+   
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
    
+    def logged_in_user
+      unless logged_in?
+      sotre_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
